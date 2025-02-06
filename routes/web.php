48e371bb77e563eae7db29a66ff5f520cb84f4a3
Rajaza12 <jakanirayane2@gamil.com>
2025-02-06 11:15:50 +0100

    Instalation breese PHP

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InscriptionController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,11 +14,18 @@ use App\Http\Controllers\InscriptionController;
 |
 */
 
-
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
-Route::get('/inscription', [InscriptionController::class, 'create'])->name('inscription.create');
-Route::post('/inscription', [InscriptionController::class, 'store'])->name('inscription.store');
-Route::get('/inscriptions', [InscriptionController::class, 'index'])->name('inscription.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
